e800304b12e2844b8cb80d91c6d20d21d34a318d
Nnamaka <nnamaka7@gmail.com>
2023-06-28 07:13:44 -0700

    see changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" Pascal Triangle """
+
 
 def pascal_triangle(n):
     """
@@ -7,29 +9,29 @@ def pascal_triangle(n):
         n - size of pascal triangle
     returns:
         list of lists
-    
+
     """
     sec = n
     main_arr = []
 
     if (sec <= 0):
         return []
-    
+
     if (sec == 1):
         return [[1]]
-    
+
     if (sec == 2):
-        return [[1],[1,1]]
-    
+        return [[1], [1, 1]]
+
     m, n = (0, 1)
-    main_arr =  [[1],[1,1]]
+    main_arr = [[1], [1, 1]]
 
     for num in range(2, sec):
         num = num + 1
         work_arr = main_arr[-1]
         small_arr = []
 
-        while( n <= num - 2):
+        while (n <= num - 2):
             small_arr.append(work_arr[m] + work_arr[n])
             m = m + 1
             n = n + 1
@@ -39,6 +41,3 @@ def pascal_triangle(n):
         m, n = (0, 1)
 
     return main_arr
-
-
-
