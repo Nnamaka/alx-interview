f50c224da67dc5c933c5201d8382295beff00997
nnamaka <nnamaka7@gmail.com>
2023-07-10 23:42:01 +0100

    time

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -21,7 +21,16 @@ def canUnlockAll(boxes):
                     keyz.update(set(boxes[j]))
                     found = True
                     break
-            if found:
+            if not found:
                 return False
 
     return True
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
