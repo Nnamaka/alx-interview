207b743e8772115c93f05211cbb44dc1e5ae563a
nnamaka <nnamaka7@gmail.com>
2023-08-11 00:04:04 +0100

    checking

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require("request");
+
+const URL = "https://swapi-api.hbtn.io/api";
+const MOVIE_ID = process.argv[2];
+const API_ENDPOINT = URL + "/films/" + MOVIE_ID;
+
+if (MOVIE_ID) {
+  request(API_ENDPOINT, (err, clbk, res) => {
+    if (!err) {
+      const charLink = JSON.parse(res).characters;
+      const charNames = charLink.map(
+        (url) =>
+          new Promise((resolve, reject) => {
+            request(url, (promiseErr, __, charactersReqBody) => {
+              if (promiseErr) {
+                reject(promiseErr);
+              }
+              resolve(JSON.parse(charactersReqBody).name);
+            });
+          })
+      );
+
+      Promise.all(charNames)
+        .then((names) => console.log(names.join("\n")))
+    }
+  });
+}
