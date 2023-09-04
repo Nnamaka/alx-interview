5d08e28b1ad627c5e470494cc63283b0b3eb7f4b
nnamaka <nnamaka7@gmail.com>
2023-09-04 11:53:20 +0100

    sharpally

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid):
     for a in range(len(grid)):
         for b in range(len(grid[0])):
 
-            if wi[0] == None and grid[a][b] == 1:
+            if wi[0] is None and grid[a][b] == 1:
                 wi[0] = b
             elif wi[0] is not None and b < wi[0] and grid[a][b] == 1:
                 wi[0] = b
@@ -24,7 +24,7 @@ def island_perimeter(grid):
     for c in range(len(grid[0])):
         for d in range(len(grid)):
 
-            if hi[0] == None and grid[d][c] == 1:
+            if hi[0] is None and grid[d][c] == 1:
                 hi[0] = d
             elif hi[0] is not None and d < hi[0] and grid[d][c] == 1:
                 hi[0] = d
@@ -34,15 +34,15 @@ def island_perimeter(grid):
 
 
     # calculate width and height
-    hi[0] = 0 if hi[0] == None else hi[0]
-    wi[0] = 0 if wi[0] == None else wi[0]
+    hi[0] = 0 if hi[0] is None else hi[0]
+    wi[0] = 0 if wi[0] is None else wi[0]
 
 
     height = (hi[1] - hi[0]) + 1
     width = (wi[1] - wi[0]) + 1
 
     # calculate perimeter
-    if height == 1 and width == 1:
+    if hi[0] == 0 and hi[1] == 0 and wi[0] == 0 and wi[1] == 0:
         perimeter = 0
     else:
         perimeter = height * 2 + width * 2
