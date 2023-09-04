6ec18412ca75159746e0702f32b47b96ba37b058
nnamaka <nnamaka7@gmail.com>
2023-09-04 22:14:58 +0100

    created algorithm

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Making Change'''
+
+def makeChange(coins, total):
+    '''Make change from list of coins'''
+    if total == 0:
+        return 0
+    
+    results = []
+
+    for a in range(len(coins)):
+
+        for b in range (len(coins)):
+            count = 0
+            t = coins[a]
+
+            while (not t >= total):
+                t += coins[b]
+                count += 1
+
+            results.append((count + 1, t))
+    
+    # check fewest no. of coins from "results"
+    smallest = float('inf')
+
+    for c in range(len(results)):
+        if results[c][0] < smallest and results[c][1] == total:
+            smallest = results[c][0]
+    
+    if smallest == float('inf'):
+        smallest = -1
+        
+    return smallest
