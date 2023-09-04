14023dacd82d67e5f6317bb61a980ff14f7182d5
nnamaka <nnamaka7@gmail.com>
2023-09-04 11:12:03 +0100

    sharpally

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,28 +4,40 @@
 
 def island_perimeter(grid):
     '''Island perimeter documentaion'''
-    max_w = 0
-    max_h = 0
+    hi = [None, 0]
+    wi = [None, 0]
 
     # width of the grid
     for a in range(len(grid)):
-        width = 0
         for b in range(len(grid[0])):
-            width += grid[a][b]
 
-        if width > max_w:
-            max_w = width
+            if wi[0] == None and grid[a][b] == 1:
+                wi[0] = b
+            elif wi[0] is not None and b < wi[0] and grid[a][b] == 1:
+                wi[0] = b
+            
+            if b > wi[1] and grid[a][b] == 1:
+                wi[1] = b
+
 
     # height of grid
     for c in range(len(grid[0])):
-        height = 0
         for d in range(len(grid)):
-            height += grid[d][c]
 
-        if height > max_h:
-            max_h = height
+            if hi[0] == None and grid[d][c] == 1:
+                hi[0] = d
+            elif hi[0] is not None and d < hi[0] and grid[d][c] == 1:
+                hi[0] = d
+            
+            if d > hi[1] and grid[d][c] == 1:
+                hi[1] = d
 
-    # calculate and return perimeter
-    perimiter = (max_h * 2) + (max_w * 2)
 
-    return perimiter
+    # calculate width and height
+    height = (hi[1] - hi[0]) + 1
+    width = (wi[1] - wi[0]) + 1
+
+    # calculate perimeter
+    perimeter = height * 2 + width * 2
+
+    return perimeter
